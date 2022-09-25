66d779a0a807ac9fdcfd9e4b279aa782c8968998
nisino25 <nisino25@gmail.com>
2022-09-25 11:19:09 +0900

    changed profiek

--- src/views/church.vue
+++ src/views/church.vue
@@ -3,23 +3,43 @@
     <h1>京都上賀茂教会の牧師紹介</h1><hr>
 
     
-    <div v-if="profiles">
-      <template v-for="(person, i) in profiles" :key="i">
-        <div style="margin-bottom: 50px">
-          {{person.position}}: {{person.name}} <br>
-          <img :src="imgSrc[i]" alt="none" class="profilePic"> <br>
-          <span>{{person.detail}}</span>
-          <hr>
-          <!-- <span style="margin-top:50px" class="description">{{getStyle(person.description)}}</span> -->
-          <span style="margin-top:50px" class="description">{{getStyle(person.description)}}</span>
-          <hr>
+    <div>
 
+      <div style="margin-bottom: 50px" class="profile">
+        主任牧師: &nbsp;<ruby>生田 香緒里 <rt>いくた かおり</rt></ruby> <br>
+        <img src="../../public/img/ikuta-san.jpg"  alt="none" class="profilePic"> <br>
+        <!-- <span></span>
+        <hr> -->
+        <!-- <span style="margin-top:50px" class="description">{{getStyle(person.description)}}</span> -->
+        <!-- <span style="margin-top:50px" class="description"></span> -->
+        
+      </div>
+      <hr>
 
-        </div>
-      </template>
+      <div style="margin-bottom: 50px" class="profile">
+        牧師: &nbsp;<ruby>粟津原 淳 <rt>あわづはら あつし</rt></ruby> <br>
+        <img src="../../public/img/awazuhara-san.jpg"  alt="none" class="profilePic"> <br>
+        <span>&nbsp;コロナ前は大人のバックパッカーを趣味としていました。イランやインドなど、西〜南アジアがお気に入りです。地元の人と一緒にバスや列車で移動するのですが、国によっては空の国内線が充実してきていて、経済の変化を感じることもあります。食事も地元の人が集う食堂や屋台が性に合っています。インドの屋台で激しい下痢をしたときは日本から持参した薬がまったく通用せず、現地の薬局で処方してもらった薬で治りました。「郷にいれば郷に従え」ということでしょうか。風邪にはアーユルヴェーダのマッサージが効きました。<br> &nbsp; 写真に写っているのは、ウズベキスタンとカザフスタンにまたがるアラル海です。かつては東北地方とほぼ同じ大きさで世界第4位の湖でしたが、現在では10〜20％くらいの面積にまで縮小しています。これはソ連が綿花栽培のために大規模な灌漑を行って、水を他の地域に流した結果です。かつては豊かな漁場でしたが、1980年代になると塩分濃度の上昇によって魚を捕れなくなりました。今は船の墓場のようになっています。人がもたらす破壊力の恐ろしさを見る思いです。<br> &nbsp;  柴犬ハルと暮らすようになってからは、我が家の半径3キロ四方の路地や公園に詳しくなりました。猫が立ち寄る所やニホントカゲの住処など、いままで見えていなかった場所に連れていってもらっています。地の果てのような遠くまで出かけるのも楽しいですが、どこにでもちょっとした発見や驚きはあるものです。</span>
+        <!-- <span style="margin-top:50px" class="description">{{getStyle(person.description)}}</span> -->
+        <!-- <span style="margin-top:50px" class="description"></span> -->
+        
+      </div>
+      <hr>
+
+      <div style="margin-bottom: 50px" class="profile">
+        牧師: &nbsp;<ruby>浜本京子<rt>はまもと きょうこ</rt></ruby> <br>
+        <img src="../../public/img/hamamoto-san.jpg"  alt="none" class="profilePic"> <br>
+        <span>
+          ２０１８年からハワイのオワフ島で、在宅ホスピスの訪問サービスの働きに関わっています。訪問看護サービスというのを聞かれたことがあると思います。そのチャプレン版です。<br> &nbsp; 本来ホスピスとは施設ではなくサービスのこと。心身ともに人生の終わりの時を平安に過ごせるよう、看護師、医師、ソーシャルワーカー、チャプレン、ボランティア等がチームとなり、患者さんやご家族を支えていきます。わたしのしごとは、お話を聴いたりお祈りしたりしながら、人々のこころを見守っていくことです。宗教の有無や違いに関係なく、どんな人でもどこにでも出かけていきます。自宅や高齢者施設、ケアホームやホームレスキャンプなど、毎日島中を車で回っています。 <br> &nbsp; 数年前のある日、自分のスマホが頼んでもないのに、勝手にわたしの歩数を数えていることがわかりました。見てびっくり、これは完全に運動不足ぎみ。それからすぐにコロナ禍が押し寄せてきて、それならば気分転換にと家のまわりを歩くようになりました。はじめはいつ家にたどり着つくのか、そればかり気になっていたのですが、いまはただ歩くことが楽しいです。ハワイの朝は美しく、空や鳥たち、草花にあいさつしながら、毎日てくてく歩いています。目に留まったものを写真に撮るのも楽しみ。毎日曜日の聖書日課ブログで写真をアップしています。良ければどうぞたずねてみてください。
+        </span>
+        <!-- <span style="margin-top:50px" class="description">{{getStyle(person.description)}}</span> -->
+        <!-- <span style="margin-top:50px" class="description"></span> -->
+        
+      </div>
+      <hr>
+
     </div>
 
-    <div class="loader" v-else></div>
     <br>
 
      
@@ -32,91 +52,76 @@
 </template>
 
 <script>
-import db from '../../firebase.js';
+// import db from '../../firebase.js';
 
 
 
 
-export default {
-  components: { 
-    // GoogleMap, 
-    // Marker,
-  },
+// export default {
+//   components: { 
+//     // GoogleMap, 
+//     // Marker,
+//   },
 
-  data(){
-    return{
-      profiles: undefined,
-      // links,
-      // link
-    }
-  },
+//   data(){
+//     return{
+//       // profiles: undefined,
+//       // links,
+//       // link
+//     }
+//   },
 
-  setup() {
-    const center = { lat: 35.05922090366774,  lng: 135.7575056779574, };
-    const markerOptions = { position: center, label: "京都上賀茂教会", title: "京都上賀茂教会" };
+//   // setup() {
+//   //   // const center = { lat: 35.05922090366774,  lng: 135.7575056779574, };
+//   //   // const markerOptions = { position: center, label: "京都上賀茂教会", title: "京都上賀茂教会" };
 
     
 
-    return { center, markerOptions };
-  },
+//   //   // return { center, markerOptions };
+//   // },
   
-  methods:{
-    test(){
-        const ref = db.collection('profiles')
-        ref.doc(`priests`).set({
-          data:[
-            {position: '主任牧師', name: '生田 香緒里', detail: '',  description: ''},
+//   methods:{
 
-            {position: '牧師', name: '粟津原 淳', detail: '粟津原　淳（あわづはら　あつし）長野県生まれ。新潟大学医療技術短大部、同志社大学院　神学研究科博士前期過程、Pacifica Graduate Institute（カリフォルニア州）と渡り歩き、現在は同志社女子大学嘱託講師（聖書）と診療放射線技師をしています。',  description: 'コロナ前は大人のバックパッカーを趣味としていました。イランやインドなど、西〜南アジアがお気に入りです。地元の人と一緒にバスや列車で移動するのですが、国によっては空の国内線が充実してきていて、経済の変化を感じることもあります。食事も地元の人が集う食堂や屋台が性に合っています。インドの屋台で激しい下痢をしたときは日本から持参した薬がまったく通用せず、現地の薬局で処方してもらった薬で治りました。「郷にいれば郷に従え」ということでしょうか。風邪にはアーユルヴェーダのマッサージが効きました。写真に写っているのは、ウズベキスタンとカザフスタンにまたがるアラル海です。かつては東北地方とほぼ同じ大きさで世界第4位の湖でしたが、現在では10〜20％くらいの面積にまで縮小しています。これはソ連が綿花栽培のために大規模な灌漑を行って、水を他の地域に流した結果です。かつては豊かな漁場でしたが、1980年代になると塩分濃度の上昇によって魚を捕れなくなりました。今は船の墓場のようになっています。人がもたらす破壊力の恐ろしさを見る思いです。 柴犬ハルと暮らすようになってからは、我が家の半径3キロ四方の路地や公園に詳しくなりました。猫が立ち寄る所やニホントカゲの住処など、いままで見えていなかった場所に連れていってもらっています。地の果てのような遠くまで出かけるのも楽しいですが、どこにでもちょっとした発見や驚きはあるものです。'},
-
-            {position: '牧師', name: '張 悦子', detail: '',  description: ''},
-
-            {position: '牧師', name: '浜本 京子 ', detail: '浜本　京子（はまもと　きょうこ）広島県生まれ。広島県生まれ。広島女学院高校で初めて聖書に出会い、同志社大学神学部・神学研究科を経て、アンドーバーニュートン神学校(マサチューセッツ州)で過ごしました。米国チャプレン協会認定チャプレン。牧会博士。',  description: '２０１８年からハワイのオワフ島で、在宅ホスピスの訪問サービスの働きに関わっています。訪問看護サービスというのを聞かれたことがあると思います。そのチャプレン版です。本来ホスピスとは施設ではなくサービスのこと。心身ともに人生の終わりの時を平安に過ごせるよう、看護師、医師、ソーシャルワーカー、チャプレン、ボランティア等がチームとなり、患者さんやご家族を支えていきます。わたしのしごとは、お話を聴いたりお祈りしたりしながら、人々のこころを見守っていくことです。宗教の有無や違いに関係なく、どんな人でもどこにでも出かけていきます。自宅や高齢者施設、ケアホームやホームレスキャンプなど、毎日島中を車で回っています。 数年前のある日、自分のスマホが頼んでもないのに、勝手にわたしの歩数を数えていることがわかりました。見てびっくり、これは完全に運動不足ぎみ。それからすぐにコロナ禍が押し寄せてきて、それならば気分転換にと家のまわりを歩くようになりました。はじめはいつ家にたどり着つくのか、そればかり気になっていたのですが、いまはただ歩くことが楽しいです。ハワイの朝は美しく、空や鳥たち、草花にあいさつしながら、毎日てくてく歩いています。目に留まったものを写真に撮るのも楽しみ。毎日曜日の聖書日課ブログで写真をアップしています。良ければどうぞたずねてみてください。'},
-          ]
-
-        })
-    },
-
-    getData(){
-      var docRef = db.collection('profiles').doc(`priests`);
+//     // getData(){
+//     //   var docRef = db.collection('profiles').doc(`priests`);
       
       
-      docRef.get().then((doc) => {
-          if (doc.exists) {
+//     //   docRef.get().then((doc) => {
+//     //       if (doc.exists) {
             
-            this.profiles =doc.data().data
-            console.log(this.profiles)
-            // console.log(this.events)
-            // this.historyArticles = JSON.parse(doc.data().data)
-            // this.getViews()
-          } else {
-              // doc.data() will be undefined in this case
-              this.events= []
-          }
-      }).catch((error) => {
-          console.log("Error getting document:", error);
-      });
-    },
+//     //         this.profiles =doc.data().data
+//     //         console.log(this.profiles)
+//     //         // console.log(this.events)
+//     //         // this.historyArticles = JSON.parse(doc.data().data)
+//     //         // this.getViews()
+//     //       } else {
+//     //           // doc.data() will be undefined in this case
+//     //           this.events= []
+//     //       }
+//     //   }).catch((error) => {
+//     //       console.log("Error getting document:", error);
+//     //   });
+//     // },
 
-    getStyle(text){
-      return text
-      // return text.substring(0,100);
-    }
+//     getStyle(text){
+//       return text
+//       // return text.substring(0,100);
+//     }
     
-  },
+//   },
 
-  mounted(){
-    this.getData()
-  },
+//   mounted(){
+//     // this.getData()
+//   },
 
-  computed: {
-    imgSrc () {
-      let list = [require("../../public/img/ikuta-san.jpg"), require("../../public/img/awazuhara-san.jpg"),require("../../public/img/hamamoto-san.jpg"),]
-      return list
-      // return require("../../public/img/awazuhara-san.jpg")
-    },
-  }
-}
+//   computed: {
+//     imgSrc () {
+//       let list = [require("../../public/img/ikuta-san.jpg"), require("../../public/img/awazuhara-san.jpg"),require("../../public/img/hamamoto-san.jpg"),]
+//       return list
+//       // return require("../../public/img/awazuhara-san.jpg")
+//     },
+//   }
+// }
 </script>
 
 <style>
@@ -175,6 +180,13 @@ export default {
 /* white-space: nowrap; */
     /* white-space: nowrap; */
   }
+
+  span{
+    text-align: left;
+    display: block;
+  }
+
+
 
 /* * --------------------------------------------- */ 
   .loader {
