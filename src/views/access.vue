083c1fd5d86151b1484c14a9973ed73af33d55e9
nisino25 <nisino25@gmail.com>
2024-08-29 02:57:36 +0900

    change the site

--- src/views/access.vue
+++ src/views/access.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="content">
     <h1>京都上賀茂教会へのアクセス</h1>
-    <img src="../../public/img/drawing.jpg" alt="" >
-    <img src="../../public/img/map.jpg" alt="">
+    <div class="grid-container">
+      <img src="../../public/img/drawing.jpg" alt="" >
+      <img src="../../public/img/map.jpg" alt="">
+    </div>
     <br><br>
 
-    <div class="" style="width: 80%; margin: auto auto">    
-      <GoogleMap api-key="AIzaSyDypz7XN6k4Q1gEj6mKRt65nAbpOelob8o" style="width: 100%; height: 500px" :center="center" :zoom="15">
-        <Marker :options="{ position: center }" />
-      </GoogleMap>
+    <div style="text-align: center">    
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3265.907687671745!2d135.75486057671435!3d35.05904697279453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001a7fb4fe056a7%3A0xdd9dcadad2c9adb3!2z5pel5pys5Z-6552j5pWZ5ZujIOS6rOmDveS4iuizgOiMguaVmeS8mg!5e0!3m2!1sja!2sjp!4v1724867038170!5m2!1sja!2sjp" width="600" height="450" style="border:0; max-width: 95vw" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div> 
     
   </div>
@@ -16,12 +16,7 @@
 </template>
 
 <script>
-import { GoogleMap, Marker } from "vue3-google-map"; 
 export default{
-  components: { 
-    GoogleMap, 
-    Marker,
-  },
 
   data(){
     return{
@@ -50,16 +45,28 @@ h1{
   text-align: center;
 }
 img{
-  width: 600px;
+  width: 100%;
+  max-width: 600px;
   height: auto;
   text-align: center;
   margin: 50px auto;
   display: block;
 }
 
+.grid-container{
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  justify-content: space-between;
+  gap: 15px;
+}
 @media (min-width:320px) and (max-width:800px)  { /* smartphones, portrait iPhone, portrait 480x320 phones (Android) */
+  .grid-container{
+    width: 100%;
+    display: block;
+  }
   img{
-    width: 90vw;
+    width: 100%;
+    max-width: 900px;
     height: auto;
     text-align: center;
     margin: 50px auto;
